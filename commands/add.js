868e46e1c8110004fa392be11ab8a415258c78fe
Francesc Xavier Caro Miro <xavicaromiro@gmail.com>
2020-01-16 20:13:39 +0100

    Promises corrected, messages improved

--- commands/add.js
+++ commands/add.js
@@ -9,57 +9,58 @@ module.exports = {
   cooldown: 2,
   description: `Adds a rule or point.`,
   async rule(args) {
-    if (args.length >= 3) {
-      Item.find({}, (err, items) => {
-        if (err) return functions.setEmbedError(embed, err);
+    return new Promise(resolve => {
+      if (args.length >= 3) {
+        Item.find({}, (err, items) => {
+          if (err) resolve(functions.setEmbedError(embed, err));
 
-        items.sort((item1, item2) => {
-          if (item1.id > item2.id) return -1;
-          if (item1.id < item2.id) return 1;
-          return 0;
+          items.sort((item1, item2) => {
+            if (item1.id > item2.id) return -1;
+            if (item1.id < item2.id) return 1;
+            return 0;
+          });
+
+          const id = items[0].id + 1;
+          const points = parseInt(args[1]);
+          const name = Array(args)
+            .slice(2)
+            .join(' ');
+
+          const item = new Item({
+            id: id,
+            name: name,
+            points: points,
+            counter: 0
+          });
+
+          item.save(err => {
+            if (err) resolve(functions.setEmbedError(embed, err));
+            resolve(embed.setDescription(`Added successfully.`).setColor(0x0ac930));
+          });
         });
-
-        const id = items[0].id + 1;
-        const points = parseInt(args[1]);
-        const name = Array(args)
-          .slice(2)
-          .join(' ');
-
-        const item = new Item({
-          id: id,
-          name: name,
-          points: points,
-          counter: 0
-        });
-
-        item.save(err => {
-          if (err) return functions.setEmbedError(embed, err);
-          return embed.setDescription(`Added successfully.`).setColor(0x0ac930);
-        });
-      });
-    } else {
-      return functions.setEmbedIncorrect(embed, module.exports.name);
-    }
+      } else {
+        resolve(functions.setEmbedIncorrect(embed, module.exports.name));
+      }
+    });
   },
   async points(args) {
-    if (args.length == 2) {
-      const id = parseInt(args[1]);
-      Item.findOneAndUpdate({ id: id }, { $inc: { counter: 1 } }, (err, items) => {
-        if (err) return functions.setEmbedError(embed, err);
-        return embed.setDescription(`Added successfully.`).setColor(0x0ac930);
-      });
-    } else {
-      return functions.setEmbedIncorrect(embed, module.exports.name);
-    }
+    return new Promise(resolve => {
+      if (args.length == 2) {
+        const id = parseInt(args[1]);
+        Item.findOneAndUpdate({ id: id }, { $inc: { counter: 1 } }, (err, items) => {
+          if (err) resolve(functions.setEmbedError(embed, err));
+          resolve(embed.setDescription(`Added successfully.`).setColor(0x0ac930));
+        });
+      } else {
+        resolve(functions.setEmbedIncorrect(embed, module.exports.name));
+      }
+    });
   },
   async execute(message, client, args) {
     let embed = new RichEmbed();
     try {
       if (args.length == 0) {
-        embed
-          .setTitle('Info')
-          .setDescription(`\`!add [rule|points] [points|ID] [description]\``)
-          .setColor(0x0aa0c9);
+        embed.setDescription(`!add [rule|points] [points|ID] [description]`).setColor(0x0aa0c9);
       } else if (args.length < 2 || !firstOptions.find(opt => opt == args[0]) || isNaN(args[1])) {
         // If there's only one arg or this one arg is not on the first options.
         embed = functions.setEmbedIncorrect(embed, module.exports.name);
--- functions/functions.js
+++ functions/functions.js
@@ -1,14 +1,11 @@
 module.exports = {
   setEmbedIncorrect: function(embed, commandName) {
-    return embed
-      .setTitle('Not correct')
-      .setDescription(`The command is not correct! Type \`!${commandName}\` or \`!help\`.`)
-      .setColor(0xffc20d);
+    return embed.setDescription(`The command is not correct! Type \`!${commandName}\` or \`!help\`.`).setColor(0xffc20d);
   },
   setEmbedError: function(embed, err) {
     embed
-      .setTitle('Error')
-      .setDescription(`Something went wrong. ${JSON.stringify(err)}`)
+      .setTitle('Oops! Something went wrong.')
+      .setDescription(`${JSON.stringify(err)}`)
       .setColor(0xc90a0a);
   }
 };
