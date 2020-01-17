683a64f922b7ed591174bf7735b3eaf10b604b9a
fxcaro <fxcaro@3digits.es>
2020-01-17 09:21:28 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,33 +1,40 @@
-const { RichEmbed } = require('discord.js');
-const functions = require('../functions/functions');
-const Item = require('../models/item');
+const { RichEmbed } = require("discord.js");
+const functions = require("../functions/functions");
+const Item = require("../models/item");
 
-const firstOptions = ['rule', 'points'];
+const firstOptions = ["rule", "points"];
 
 async function rule(args, embed, message) {
   return new Promise(resolve => {
     if (args.length >= 3) {
-      message.channel.send('args length okay');
+      message.channel.send("args length okay");
       Item.find({}, (err, items) => {
-        if (err || !items || items.length == 0) resolve(functions.setEmbedError(embed, err));
+        message.channel.send(`items: ${items}`);
+        if (err || !items) resolve(functions.setEmbedError(embed, err));
+        const arrayItems = Array(items);
+        message.channel.send(`arrayItems: ${arrayItems}`);
+        if (arrayItems.length == 0)
+          resolve(functions.setEmbedError(embed, err));
 
-        message.channel.send(`Item.find okay: ${items.join(', ')}`);
-
-        items.sort((item1, item2) => {
+        arrayItems.sort((item1, item2) => {
           if (item1.id > item2.id) return -1;
           if (item1.id < item2.id) return 1;
           return 0;
         });
 
-        message.channel.send(`items.sort okay: ${items.join(', ')}`);
+        message.channel.send(`arrayItems.sort okay: ${arrayItems.join(", ")}`);
 
-        const id = parseInt(items[0].id) + 1;
+        const id = parseInt(arrayItems[0].id) + 1;
         const points = parseInt(args[1]);
         const name = Array(args)
           .slice(2)
-          .join(' ');
+          .join(" ");
 
-        resolve(embed.setDescription(`id: ${id}, points: ${points}, name: ${name}`).setColor(0x0ac930));
+        resolve(
+          embed
+            .setDescription(`id: ${id}, points: ${points}, name: ${name}`)
+            .setColor(0x0ac930)
+        );
 
         const item = new Item({
           id: id,
@@ -38,7 +45,9 @@ async function rule(args, embed, message) {
 
         item.save(err => {
           if (err) resolve(functions.setEmbedError(embed, err));
-          resolve(embed.setDescription(`Added successfully.`).setColor(0x0ac930));
+          resolve(
+            embed.setDescription(`Added successfully.`).setColor(0x0ac930)
+          );
         });
       });
     } else {
@@ -51,10 +60,16 @@ async function points(args, embed) {
   return new Promise(resolve => {
     if (args.length == 2) {
       const id = parseInt(args[1]);
-      Item.findOneAndUpdate({ id: id }, { $inc: { counter: 1 } }, (err, items) => {
-        if (err) resolve(functions.setEmbedError(embed, err));
-        resolve(embed.setDescription(`Added successfully.`).setColor(0x0ac930));
-      });
+      Item.findOneAndUpdate(
+        { id: id },
+        { $inc: { counter: 1 } },
+        (err, items) => {
+          if (err) resolve(functions.setEmbedError(embed, err));
+          resolve(
+            embed.setDescription(`Added successfully.`).setColor(0x0ac930)
+          );
+        }
+      );
     } else {
       resolve(functions.setEmbedIncorrect(embed, module.exports.name));
     }
@@ -62,21 +77,27 @@ async function points(args, embed) {
 }
 
 module.exports = {
-  name: 'add',
+  name: "add",
   cooldown: 2,
   description: `Adds a rule or point.`,
   async execute(message, client, args) {
     let embed = new RichEmbed();
     try {
       if (args.length == 0) {
-        embed.setDescription(`!add [rule|points] [points|ID] [description]`).setColor(0x0aa0c9);
-      } else if (args.length < 2 || !firstOptions.find(opt => opt == args[0]) || isNaN(args[1])) {
+        embed
+          .setDescription(`!add [rule|points] [points|ID] [description]`)
+          .setColor(0x0aa0c9);
+      } else if (
+        args.length < 2 ||
+        !firstOptions.find(opt => opt == args[0]) ||
+        isNaN(args[1])
+      ) {
         // If there's only one arg or this one arg is not on the first options.
         embed = functions.setEmbedIncorrect(embed, module.exports.name);
-      } else if (args[0] == 'points') {
+      } else if (args[0] == "points") {
         embed = await points(args, embed);
       } else {
-        message.channel.send('rule detected');
+        message.channel.send("rule detected");
         embed = await rule(args, embed, message);
       }
     } catch (err) {
